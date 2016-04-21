aceb3885a7e3539488d42407dee0e6e9e0dc2b67
priyank5590 <vyaspriyank5@gmail.com>
2016-04-21 07:45:57 -0700

    js added

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,8 +1,12 @@
-function changeImage() {
-    var image = document.getElementById('click');
-    if (image.src.match("bulbon")) {
-        image.src = "pic_bulboff.gif";
-    } else {
-        image.src = "pic_bulbon.gif";
-    }
-}
+(function () {
+		
+		diceRoll();
+		
+		var diceleft = document.getElementById("diceleft");
+		var diceright = document.getElementById("diceright");
+		
+		roll.addEventListener("click", function(){
+		
+		});
+
+})();
